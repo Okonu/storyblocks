b4ac4c9c2e9552af937126223f6a3bd2ed04c907
Okonu <ianyakundi015@gmail.com>
2023-01-04 00:48:52 +0300

    exphbs template engine

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); //for logging, showing requests made in the console
+const exphbs = require('express-handlebars'); //UI template engine
 const connectDB= require('./config/db');
 
 // load config file
@@ -8,6 +10,14 @@ dotenv.config({path: './config/config.env'})
 connectDB()
 
 const app =  express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')) 
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine ({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000
 
