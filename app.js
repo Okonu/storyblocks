828b4fb643991c6a440d608fa1f4008a48caddd9
Okonu <ianyakundi015@gmail.com>
2023-01-03 05:13:52 +0300

    updated files

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ dotenv.config({path: './config/config.env'})
 
 const app =  express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+
+
+
+
+// PORT = 3000
+
+// MONGO_URI = mongodb+srv://Okonu:IOkonu@1999@cluster0.64glq.mongodb.net/?retryWrites=true&w=majority 
