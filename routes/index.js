5b5d7f75f9235ad0766ca8a2ab16ecbfa4cf5b1f
Okonu <ianyakundi015@gmail.com>
2023-01-04 01:45:43 +0300

    dashboard and login render

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ const router = express.Router()
 //Login/Landing page
 //route: GET/
 router.get('/', (req, res) =>{
-    res.send('Login')
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
 //dashboard
 //route: GET/dashboard
 router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
+    res.render('dashboard')
 })
 
 module.exports = router
