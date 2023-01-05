f995ded36760adc306b00ba2890130cf734a5c35
Okonu <ianyakundi015@gmail.com>
2023-01-05 16:57:12 +0300

    auth, passport authenticate, failure redirect

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+// Auth with Google
+//route: GET/ /auth/google
+router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+
+//Google auth callback
+//route: GET /auth/google/callback
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}))
+
+module.exports = router 
