6d48993d2eaa29162e2a68fe3943dac31c15f8f6
Okonu <ianyakundi015@gmail.com>
2023-01-05 16:25:49 +0300

    serialize deserialize refactor

--- config/passport.js
+++ config/passport.js
@@ -12,13 +12,12 @@ module.exports = function(passport){
         console.log(profile)
     }))
 
-    passport.serializeUser(function(user, done){
-        done(null, user.id);
-    });
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
 
-        passport.deserializeUser(function(id, done){
-            User.findById(id, function(err, user){
-                done(err, user)
-            });
+        passport.deserializeUser((id, done) => {
+            User.findById(id, (err, user) =>
+                done(err, user))
         });
 }
