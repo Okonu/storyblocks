57aa15cfde99c6d3398e46c65b84c224eb82782b
Okonu <ianyakundi015@gmail.com>
2023-01-05 14:32:46 +0300

    passport middlewares

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan'); //for logging, showing requests made in the console
 const exphbs = require('express-handlebars'); //UI template engine
+const passport = require('passport');
+const session = require('express-session')
 const connectDB= require('./config/db');
 
 // load config file
 dotenv.config({path: './config/config.env'})
+
+//passport config
+require('./config/passport')(passport) 
 
 connectDB()
 
@@ -14,12 +19,23 @@ const app =  express()
 
 //logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev')) 
+    app.use(morgan('dev'))  
 }
 
 // Handlebars
 app.engine('.hbs', exphbs.engine ({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,          //don't save a session if nothing is modified
+    saveUninitialized: false, //don't create a session untill something is initialized
+  }))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session()) 
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
